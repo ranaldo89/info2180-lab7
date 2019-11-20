b42cdb004e82e52de553ae0db8bec8bbeacbae18
Ubuntu <ubuntu@ip-172-31-89-145.ec2.internal>
2019-11-20 20:02:53 +0000

    files added

---
+++ world.php
@@ -0,0 +1,19 @@
+<?php
+$country = $_GET['country'];
+$all = $_GET['all'];
+$host = getenv('IP');
+$username = 'lab7_user';
+$password = 'September';
+$dbname = 'world';
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+$stmt = $conn->query("SELECT * FROM countries");
+
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<ul>
+<?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>
